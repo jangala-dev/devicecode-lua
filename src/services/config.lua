63677f6d5daae2be65cfcf6dd6f71a99daba1ca0
Richard Thanki <richardthanki@gmail.com>
2025-03-04 10:44:10 +0000

    corrects  and  modules to work with new bus api

--- src/services/config.lua
+++ src/services/config.lua
@@ -1,19 +1,20 @@
 local file = require 'fibers.stream.file'
 local log = require 'log'
 local json = require 'dkjson'
+local new_msg = require 'bus'.new_msg
 
 local config_service = {}
 config_service.__index = config_service
 
-function config_service:start(rootctx, bus_connection)
+function config_service:start(rootctx, conn)
     log.trace("Starting Config Service")
-    self.bus_connection = bus_connection
+    self.conn = conn
     local config_file = assert(file.open("configs/"..rootctx:value("device")..".json"))
     local raw_config = config_file:read_all_chars()
     local config = json.decode(raw_config)
     for k, v in pairs(config) do
         log.trace("Config: publishing config for: ", k)
-        self.bus_connection:publish({topic="config/"..k, payload=json.encode(v), retained=true})
+        self.conn:publish(new_msg({ topic = { "config", k }, payload = json.encode(v), { retained = true } }))
     end
 end
 
