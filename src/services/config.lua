5e2c9fcd13d173396c639eeebf2f277875a1aa81
Ryan Slater <ryan.slater157@gmail.com>
2025-05-13 08:48:46 +0000

    Added endpoint renaming, change configs to be more explicit with endpoints

--- src/services/config.lua
+++ src/services/config.lua
@@ -1,4 +1,5 @@
 local file = require 'fibers.stream.file'
+local fiber = require 'fibers.fiber'
 local log = require 'log'
 local json = require 'dkjson'
 local new_msg = require('bus').new_msg
@@ -8,16 +9,34 @@ local config_service = {
 }
 config_service.__index = config_service
 
-function config_service:start(rootctx, conn)
-    log.trace("Starting Config Service")
-    self.conn = conn
-    local config_file = assert(file.open("configs/"..rootctx:value("device")..".json"))
+local function publish_config(ctx, conn)
+    -- publish service configs
+    local config_file = assert(file.open("configs/" .. ctx:value("device") .. ".json"))
     local raw_config = config_file:read_all_chars()
     local config = json.decode(raw_config)
     for k, v in pairs(config) do
         log.trace("Config: publishing config for: ", k)
-        self.conn:publish(new_msg({ "config", k }, v, { retained = true }))
+        conn:publish(new_msg({ "config", k }, v, { retained = true }))
     end
+
+    -- publish mainflux configs
+    local mainflux_config_file = file.open("/data/configs/mainflux.cfg")
+    if not mainflux_config_file then
+        log.warn("Failed to open mainflux config file, cloud telemetry will not work")
+        return
+    end
+    local raw_mainflux_config = mainflux_config_file:read_all_chars()
+    local mainflux_config = json.decode(raw_mainflux_config)
+    log.trace("Config: publishing config for: mainflux")
+    conn:publish(new_msg({ "config", "mainflux" }, mainflux_config, { retained = true }))
+end
+
+function config_service:start(rootctx, conn)
+    log.trace("Starting Config Service")
+
+    fiber.spawn(function()
+        publish_config(rootctx, conn)
+    end)
 end
 
 return config_service
--- src/services/metrics.lua
+++ src/services/metrics.lua
@@ -6,6 +6,9 @@ local op = require "fibers.op"
 local log = require 'log'
 local trie = require 'trie'
 local sc = require 'fibers.utils.syscall'
+local json = require 'cjson'
+local request = require 'http.request'
+local senml = require 'services.metrics.senml'
 local unpack = table.unpack or unpack
 
 ---@class metrics_service
@@ -20,8 +23,8 @@ metrics_service.__index = metrics_service
 
 --- this is the only publish protocol
 --- (for testing purposes)
---- @param val table
-function metrics_service:_log_publish(val)
+--- @param data table
+function metrics_service:_log_publish(data)
     local function print_recursive(t, indent)
         for k, v in pairs(t) do
             if type(v) == "table" then
@@ -32,7 +35,88 @@ function metrics_service:_log_publish(val)
             end
         end
     end
-    print_recursive(val, 0)
+    print_recursive(data, 0)
+end
+
+local function validate_http_config(config)
+    if not config then
+        return false, "No cloud config set"
+    elseif not config.url then
+        return false, "No cloud url set"
+    elseif not config.mainflux_key or not config.mainflux_channels then
+        return false, "No mainflux config set"
+    end
+    return true, nil
+end
+
+function metrics_service:_http_publish(data)
+    local senml_list = senml.encode_r("", data)
+    local body = json.encode(senml_list)
+    local valid_config, config_err = validate_http_config(self.cloud_config)
+    if not valid_config then
+        log.error(string.format(
+            "%s - %s: HTTP publish failed, reason: %s",
+            self.ctx:value('service_name'),
+            self.ctx:value('fiber_name'),
+            config_err
+        ))
+        return
+    end
+    local channel_id
+    for _, channel in ipairs(self.cloud_config.mainflux_channels) do
+        if string.find(channel.name, "data") then
+            channel_id = channel.id
+            break
+        end
+    end
+    if channel_id == nil then
+        log.error(string.format(
+            "%s - %s: HTTP publish failed, reason: no channel id found",
+            self.ctx:value('service_name'),
+            self.ctx:value('fiber_name')
+        ))
+        return
+    end
+    local uri = string.format("%s/http/channels/%s/messages",
+        self.cloud_config.url,
+        channel_id
+    )
+    local req = request.new_from_uri(uri)
+    req.headers:upsert(":method", "POST")
+    req.headers:upsert("authorization", "Thing " .. self.cloud_config.mainflux_key)
+    req.headers:upsert("content-type", "senml+json")
+    req:set_body(body)
+    req.headers:delete("expect")
+    local response_headers, _ = req:go()
+
+    if not response_headers then
+        log.error(string.format(
+            "%s - %s: HTTP publish failed, reason: %s",
+            self.ctx:value('service_name'),
+            self.ctx:value('fiber_name'),
+            "No response headers"
+        ))
+        return
+    elseif response_headers:get(":status") ~= "202" then
+        local header_msgs = ""
+        for k, v in pairs(response_headers:each()) do
+            header_msgs = string.format("%s\n\t%s: %s", header_msgs, k, v)
+        end
+
+        log.debug(string.format(
+            "%s - %s: HTTP publish failed, header responses: %s",
+            self.ctx:value('service_name'),
+            self.ctx:value('fiber_name'),
+            header_msgs
+        ))
+    else
+        log.info(string.format(
+            "%s - %s: HTTP publish success, response: %s",
+            self.ctx:value('service_name'),
+            self.ctx:value('fiber_name'),
+            response_headers:get(":status")
+        ))
+    end
 end
 
 ---iterates over a table of data to be published
@@ -62,11 +146,14 @@ end
 function metrics_service:_build_metric_pipeline(endpoint, process_config)
     -- processing blocks take a value and output another value
     -- build up a processing pipeline between the cache and publish steps
-    local process, process_err = processing.new_process_pipeline(process_config or self.default_process)
+    local process, process_err = processing.new_process_pipeline()
     if process_err or not process then
         return nil, process_err
     end
-    for _, process_block_config in ipairs(process_config or self.default_process) do
+    if process_config == nil then
+        return nil, "process config is nil"
+    end
+    for _, process_block_config in ipairs(process_config) do
         local process_type = process_block_config.type
         if process_type == nil then
             return nil, string.format('Metric config [%s] has process with no type', endpoint)
@@ -91,20 +178,21 @@ function metrics_service:_build_metric_pipeline(endpoint, process_config)
     return process, nil
 end
 
----check that an overriding endpoint does not conflict with the original endpoint
----@param endpoint string[]
----@param override_endpoint string[]
----@return boolean
-local function valid_override_endpoint(endpoint, override_endpoint)
-    for i, endpart in ipairs(endpoint) do
-        if i == #endpoint and endpart == '#' then return true end
-        if endpart ~= override_endpoint[i] and endpart ~= '+' then
-            return false
+local function merge_config(base_config, override_vals)
+    if not base_config then
+        return override_vals
+    end
+
+    for k, v in pairs(override_vals) do
+        if type(v) == 'table' and type(base_config[k]) == 'table' then
+            base_config[k] = merge_config(base_config[k], v)
+        else
+            base_config[k] = v
         end
     end
-    return true
-end
 
+    return base_config
+end
 ---use config to build cache and processing pipelines
 ---@param config table
 function metrics_service:_handle_config(config)
@@ -121,9 +209,9 @@ function metrics_service:_handle_config(config)
         return
     end
 
+    self.cloud_config = merge_config(self.cloud_config, { url = config.cloud_url })
     -- clean up any old metric pipelines
     self.metric_ops = {}
-    self.default_process = config.default_process
     if config.publish_cache then
         local period = config.publish_cache.period
         local publish_cache, cache_err = timed_cache.new(period, sc.monotime)
@@ -142,7 +230,7 @@ function metrics_service:_handle_config(config)
     for endpoint, metric_config in pairs(config.collections) do
         -- could our bus just take tables or strings so I don't have to do this?
         local sub_topic = self.str_trie:_key_to_tokens(endpoint)
-        local metric_sub = self.bus_conn:subscribe(sub_topic)
+        local metric_sub = self.conn:subscribe(sub_topic)
 
         -- protocol defines the publish method to be used
         local protocol = metric_config.protocol
@@ -158,25 +246,12 @@ function metrics_service:_handle_config(config)
             return
         end
 
-        -- some specific endpoints require different processing, so we override
-        -- wildcards and build specific pipelines for these cases
-        local overrides = {}
-        for override_endpoint, override_config in pairs(metric_config.sub_collections or {}) do
-            local parts_ovrride_endpoint = self.str_trie:_key_to_tokens(override_endpoint)
-            -- the overriden endpoint can only change wildcard fields,
-            -- so check validity
-            if valid_override_endpoint(sub_topic, parts_ovrride_endpoint) then
-                local pipeline, pipeline_err = self:_build_metric_pipeline(override_endpoint, override_config)
-                if pipeline_err then
-                    log.error(pipeline_err)
-                    return
-                end
-                overrides[override_endpoint] = pipeline
-            else
-                log.error(string.format("Invalid override for %s onto %s", override_endpoint, endpoint))
-            end
+        local endpoint_rename = metric_config.rename
+        if type(endpoint_rename) ~= 'table' and type(endpoint_rename) ~= 'nil' then
+            log.warn(string.format('Metric config [%s] rename is not of expected type: table', endpoint))
+        elseif type(endpoint_rename) == 'table' then
+            table.insert(endpoint_rename, 1, protocol)
         end
-
         -- Now we wrap our bus endpoint in a function to
         -- run our processing pipline in a cache
         -- the cache is needed to give each endpoint of a wildcard
@@ -192,18 +267,13 @@ function metrics_service:_handle_config(config)
                 log.debug('Metric is nil for endpoint: ' .. metric_endpoint)
                 return
             end
-            -- check for a custom pipeline otherwise apply default
-            local process = overrides[metric_endpoint]
-            if process == nil then
-                process = default_pipeline
-            end
 
             local val, short_circuit, err
             -- either set or update the value into our pipeline
             if self.cache:has_key(metric_msg.topic) then
                 val, short_circuit, err = self.cache:update(metric_msg.topic, metric)
             else
-                val, short_circuit, err = self.cache:set(metric_msg.topic, metric, process)
+                val, short_circuit, err = self.cache:set(metric_msg.topic, metric, default_pipeline)
             end
 
             if err then
@@ -213,7 +283,8 @@ function metrics_service:_handle_config(config)
             -- if the pipeline completed with no early exit we can update our publish cache
             if not short_circuit then
                 table.insert(metric_msg.topic, 1, protocol)
-                self.publish_cache:set(metric_msg.topic, val)
+                local cache_topic = endpoint_rename or metric_msg.topic
+                self.publish_cache:set(cache_topic, val)
             end
         end)
 
@@ -222,10 +293,12 @@ function metrics_service:_handle_config(config)
 end
 
 ---setup initial cache and config then loop over config, metrics and timed cache operations
-function metrics_service:_main()
+function metrics_service:_main(ctx)
+    self.ctx = ctx
     self.cache = reactive_cache.new()
 
-    local config_sub = self.bus_conn:subscribe({'config', 'metrics'})
+    local config_sub = self.conn:subscribe({ 'config', 'metrics' })
+    local cloud_config_sub = self.conn:subscribe({ 'config', 'mainflux' })
 
     local initial_config, iconfig_err = config_sub:next_msg_with_context_op(self.ctx):perform()
     if iconfig_err then
@@ -234,7 +307,7 @@ function metrics_service:_main()
     end
     self:_handle_config(initial_config.payload)
 
-    -- local device_info_sub = self.bus_conn:subscribe({'system', 'device', 'idenitity'}) idk what this will be but
+    -- local device_info_sub = self.conn:subscribe({'system', 'device', 'idenitity'}) idk what this will be but
     -- it is a reminder to get system info that canopy will need for reporting
 
     while not self.ctx:err() do
@@ -243,24 +316,28 @@ function metrics_service:_main()
             config_sub:next_msg_op():wrap(function (config_msg)
                 self:_handle_config(config_msg.payload)
             end),
+            cloud_config_sub:next_msg_op():wrap(function(config_msg)
+                self.cloud_config = merge_config(self.cloud_config, config_msg.payload)
+            end),
             self.publish_cache:get_op():wrap(function(data)
                 self:_publish_all(data)
             end),
             unpack(self.metric_ops)
         ):perform()
     end
+    config_sub:unsubscribe()
+    cloud_config_sub:unsubscribe()
     log.info("Metrics Service Ending")
 end
 
 ---Creates the metrics fiber
 ---@param ctx Context
----@param bus_connection Connection
-function metrics_service:start(ctx, bus_connection)
+---@param conn Connection
+function metrics_service:start(ctx, conn)
     log.info("Metrics Service Starting")
-    self.ctx = ctx
-    self.bus_conn = bus_connection
-    service.spawn_fiber('Metrics Fiber', bus_connection, ctx, function ()
-        metrics_service:_main()
+    self.conn = conn
+    service.spawn_fiber('Main Fiber', conn, ctx, function(metrics_ctx)
+        metrics_service:_main(metrics_ctx)
     end)
 end
 
