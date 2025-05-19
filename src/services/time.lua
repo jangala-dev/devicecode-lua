8a3220c01f446dbe0d4d6d2cbb733198c96ff377
Richard Thanki <richardthanki@gmail.com>
2025-05-19 02:54:01 +0000

    working time module

--- src/services/time.lua
+++ src/services/time.lua
@@ -1,97 +1,83 @@
 -- Importing necessary modules
 local fiber = require "fibers.fiber"
-local socket = require 'fibers.stream.socket'
-local sc = require 'fibers.utils.syscall'
-local sleep = require 'fibers.sleep'
+local op = require "fibers.op"
+local exec = require 'fibers.exec'
+local alarm = require 'fibers.alarm'
+local cjson = require "cjson.safe"
+local log = require 'log'
+local new_msg = require('bus').new_msg
 
 local time_service = {
     name = "time"
 }
 time_service.__index = time_service
 
--- Define the path for the Unix domain socket
-local sockname = '/tmp/ntpd-sock'
-
-local hotplug_handler = [[
-# Build the JSON string
-JSON_STRING="{\"ACTION\": \"$ACTION\", \"freq_drift_ppm\": \"$freq_drift_ppm\", \"offset\": \"$offset\", \"stratum\": \"$stratum\", \"poll_interval\": \"$poll_interval\"}"
-
-# Launch the Lua script with the JSON string as an argument
-luajit /etc/hotplug.d/ntp/socket_send.lua "/tmp/ntpd-sock" "$JSON_STRING"
-]]
-
-local socket_send = [[
-package.path = "../?.lua;" .. package.path .. ";/usr/lib/lua/?.lua;/usr/lib/lua/?/init.lua"
-
--- Importing necessary modules
-local fiber = require "fibers.fiber"
-local socket = require 'fibers.stream.socket'
-
--- Install a polling I/O handler from the fibers library
-require("fibers.pollio").install_poll_io_handler()
-
--- The first argument is the JSON string
-local json_str = arg[2]
-print(json_str)
-
--- Define the path for the Unix domain socket
-local sockname = arg[1]
-
-fiber.spawn(function ()
-    local client = socket.connect_unix(sockname)
-    client:setvbuf('no')
-
-    client:write(json_str)
-
-    client:close()
-    fiber.stop()
-end)
-
--- Start the main fiber loop
-fiber.main()
-]]
-
-local function start_ntp_monitor(ctx, sockname)
-    -- Remove the socket file if it already exists to avoid 'address already in use' errors
-    sc.unlink(sockname)
-
-    -- Create and start listening on the Unix domain socket
-    local server = assert(socket.listen_unix(sockname))
-
-    while true do
-        -- Accept a new connection
-        local peer, err = assert(server:accept())
-
-        if not peer then
-            print("Error accepting connection:", err)
-            break
-        end
-
-        -- Spawn a new fiber for each connection to handle client communication
-        fiber.spawn(function()
-            while true do
-                -- Read a line from the connected client
-                local rec = peer:read_line()
-
-                -- If a line is received, process it
-                if rec then
-                    print("received:", rec)
-                else
-                    -- If no data is received (client closed the connection), break the loop
-                    print("exiting")
-                    break
-                end
-            end
-            -- Close the connection to the client
-            peer:close()
-        end)
+local function ntpd_monitor(ctx)
+    -- now we start a continuous loop to monitor for changes in interface state
+    local cmd = exec.command("ubus", "listen", "hotplug.ntp")
+    local stdout = cmd:stdout_pipe()
+    if not stdout then
+        log.error("TIME: could not create stdout pipe for ubus listen")
+        return
     end
-    -- After the server is stopped, remove the socket file and stop the fiber
-    sc.unlink(sockname)
+
+    local err = cmd:start()
+    if err then
+        log.error("TIME: ubus listen failed:", err)
+        return
+    end
+
+    local err = exec.command("/etc/init.d/sysntpd", "restart"):run()
+    if err then
+        log.error("TIME: ubus listen failed:", err)
+        return
+    end
+
+    local process_ended = false
+
+    while not ctx:err() and not process_ended do
+        op.choice(
+            stdout:read_line_op():wrap(function(line)
+                if not line then
+                    log.error("TIME: ubus listen unexpectedly exited")
+                    process_ended = true
+                else
+                    local event, err = cjson.decode(line)
+                    if err then
+                        log.error("TIME: ubus listen line decode failed:", err)
+                        return
+                    end
+                    log.debug("TIME: ntpd hotplug event received!")
+                    if event["hotplug.ntp"] and event["hotplug.ntp"].stratum then
+                        if event["hotplug.ntp"].stratum ~= 16 then
+                            log.debug("TIME: ntp time synced!")
+                            time_service.bus_connection:publish(new_msg({ "config", "ntp_synced" }, true,
+                                { retained = true }))
+                            alarm.clock_synced()
+                        else
+                            log.debug("TIME: ntp time desynced")
+                            time_service.bus_connection:publish(new_msg({ "config", "ntp_synced" }, false,
+                                { retained = true }))
+                            alarm.clock_desynced()
+                        end
+                    else
+                        log.warn("Received unknown")
+                    end
+                end
+            end),
+            ctx:done_op():wrap(function()
+                cmd:kill()
+            end)
+        ):perform()
+    end
+    cmd:wait()
+    stdout:close()
 end
 
 function time_service:start(root_ctx, bus_connection)
+    alarm.install_alarm_handler()
     self.bus_connection = bus_connection
+    fiber.spawn(function() ntpd_monitor(root_ctx) end)
 end
 
 return time_service
