ebf6443debf2f707c200bd2be47d5be7db7412c7
Ryan Name <ryan.slater157@gmail.com>
2025-06-05 12:23:17 +0100

    HAL  stores devices and capabilities using unchanging identites, HAL swapped info channels for one info queue, Modem driver changed slot monitoring to be more consistant,

--- src/services/hal.lua
+++ src/services/hal.lua
@@ -1,11 +1,9 @@
 local modem_manager = require "services.hal.managers.modemcard"
-local structures = require "services.hal.structures"
 local fiber = require "fibers.fiber"
 local queue = require "fibers.queue"
 local op = require "fibers.op"
 local service = require "service"
-local bus = require "bus"
-local new_msg = bus.new_msg
+local new_msg = require("bus").new_msg
 local log = require "services.log"
 local unpack = table.unpack or unpack
 
@@ -13,73 +11,51 @@ local unpack = table.unpack or unpack
 local hal_service = {
     name = "hal",
     capabilities = {},
-    devices = {
-        index = {},
-        id = {}
-    },
-    events = structures.new_event_list(),
-    device_event_q = queue.new(),
+    devices = {},
+    capability_info_q = queue.new(20),
+    device_event_q = queue.new(10),
     modem_manager_instance = modem_manager.new()
 }
 hal_service.__index = hal_service
 
-local function get_stream_handler(conn, endpoint_type)
-    if endpoint_type == 'multiple' then
-        return function(topic, msg)
-            conn:publish_multiple(
-                topic,
-                msg,
-                { retained = true }
-            )
-        end
-    elseif endpoint_type == 'single' then
-        return function(topic, msg)
-            conn:publish(new_msg(
-                topic,
-                msg,
-                { retained = true }
-            ))
-        end
-    else
-        return nil, string.format("invalid endpoint type '%s'", endpoint_type)
-    end
-end
 ---Adds a device, its capabilities and events to the service
 ---@param device table
 ---@param capabilities table
 ---@return table device
+---@return string? error
 function hal_service:_register_device(device, capabilities)
-    if not self.devices.index[device.type] then
-        self.devices.index[device.type] = structures.new_tracker()
-        self.devices.id[device.type] = {}
+    if not self.devices[device.type] then
+        self.devices[device.type] = {}
     end
-    local device_tracker = self.devices.index[device.type]
     device.capabilities = {}
     for cap_name, cap in pairs(capabilities) do
-        if not self.capabilities[cap_name] then
-            self.capabilities[cap_name] = structures.new_tracker()
-        end
-
-        local cap_idx = self.capabilities[cap_name]:add(cap.control)
-        device.capabilities[cap_name] = cap_idx
-
-        for _, info_stream in ipairs(cap.info_streams) do
-            local stream_handler, handler_err = get_stream_handler(self.conn, info_stream.endpoints)
-            if handler_err then
-                log.debug("Device Event: " .. handler_err)
-            else
-                local event_op = info_stream.channel:get_op():wrap(function(msg)
-                    stream_handler(
-                        { 'hal', 'capability', cap_name, cap_idx, 'info', info_stream.name },
-                        msg
-                    )
-                end)
-                self.events:add({ cap_name, cap_idx }, event_op)
+        if not cap.id then
+            log.error(string.format(
+                "Device Event: capability '%s' for device '%s' with id '%s' does not have an id",
+                cap_name, device.type, device.id
+            ))
+        else
+            if not self.capabilities[cap_name] then
+                self.capabilities[cap_name] = {}
             end
+
+            if self.capabilities[cap_name][cap.id] then
+                log.debug(string.format(
+                    "Device Event: capability '%s' with id '%s' already exists, overwriting",
+                    cap_name, cap.id
+                ))
+            end
+            self.capabilities[cap_name][cap.id] = cap.control
+            device.capabilities[cap_name] = cap.id
         end
     end
-    device.index = device_tracker:add(device)
-    self.devices.id[device.type][device.id] = device
+    if self.devices[device.type][device.id] then
+        log.debug(string.format(
+            "Device Event: device '%s' with id '%s' already exists, overwriting",
+            device.type, device.id
+        ))
+    end
+    self.devices[device.type][device.id] = device
 
     return device
 end
@@ -90,20 +66,18 @@ end
 ---@return string? error
 function hal_service:_unregister_device(device)
     -- retrieve full device
-    device = self.devices.id[device.type][device.id]
+    device = self.devices[device.type] and self.devices[device.type][device.id]
     if not device then return nil, "removed device does not exist" end
-
-    -- remove device
-    local device_tracker = self.devices.index[device.type]
-    if device_tracker then
-        device_tracker:remove(device.index)
-    end
-    self.devices.id[device.type][device.id] = nil
+    self.devices[device.type][device.id] = nil
 
     -- remove capabilities and event channels
-    for cap_name, cap_index in pairs(device.capabilities) do
-        self.events:remove({ cap_name, cap_index })
-        self.capabilities[cap_name]:remove(cap_index)
+    for cap_name, cap_id in pairs(device.capabilities) do
+        self.capabilities[cap_name][cap_id] = nil
+        self.conn:publish(new_msg(
+            { 'hal', 'capability', cap_name, cap_id, '#' },
+            nil,
+            { retained = true }
+        ))
     end
 
     return device
@@ -166,30 +140,31 @@ function hal_service:_handle_device_connection_event(connection_event)
             return
         end
         device = full_device
+        -- since we now have the original device instance, change to disconnected
         device.connected = false
     end
 
     -- broadcast the capabilities and device connections/removals on the bus
 
-    for cap_name, cap_index in pairs(device.capabilities) do
+    for cap_name, cap_id in pairs(device.capabilities) do
         self.conn:publish(new_msg(
-            { 'hal', 'capability', cap_name, cap_index },
+            { 'hal', 'capability', cap_name, cap_id },
             {
                 connected = device.connected,
                 type = cap_name,
-                index = cap_index,
-                device = { type = device.type, index = device.index }
+                index = cap_id,
+                device = { type = device.type, index = device.id }
             },
             { retained = true }
         ))
     end
 
     self.conn:publish(new_msg(
-        { 'hal', 'device', device.type, device.index },
+        { 'hal', 'device', device.type, device.id },
         {
             connected = device.connected,
             type = device.type,
-            index = device.index,
+            index = device.id,
             -- in this case device refers to the specific type of hardware
             identity = device.data.device
         },
@@ -209,7 +184,7 @@ function hal_service:_handle_capability_control(request)
         return
     end
 
-    local instance = cap:get(instance_id)
+    local instance = cap[instance_id]
     if instance == nil then
         local msg = new_msg({ request.reply_to }, { result = nil, err = 'capability instance does not exist' })
         self.conn:publish(msg)
@@ -235,6 +210,54 @@ function hal_service:_handle_capability_control(request)
     end)
 end
 
+function hal_service:_handle_capbility_info(data)
+    if not data then return end
+
+    if not data.type then
+        log.error(string.format(
+            '%s - %s: Capability info message does not have a type field',
+            self.ctx:value("service_name"),
+            self.ctx:value("fiber_name")
+        ))
+        return
+    end
+
+    if not data.id then
+        log.error(string.format(
+            '%s - %s: Capability info message does not have an id field',
+            self.ctx:value("service_name"),
+            self.ctx:value("fiber_name")
+        ))
+        return
+    end
+
+    if not data.endpoints then
+        log.error(string.format(
+            '%s - %s: Capability info message must define an endpoints field ("single" or "multiple")',
+            self.ctx:value("service_name"),
+            self.ctx:value("fiber_name")
+        ))
+        return
+    end
+
+    local sub_topic = data.sub_topic or {}
+    local topic = { 'hal', 'capability', data.type, data.id, 'info', unpack(sub_topic) }
+
+    if data.endpoints == 'single' then
+        self.conn:publish(new_msg(
+            topic,
+            data.info,
+            { retained = true }
+        ))
+    elseif data.endpoints == 'multiple' then
+        self.conn:publish_multiple(
+            topic,
+            data.info,
+            { retained = true }
+        )
+    end
+end
+
 ---Handles capability and device control, and connection events
 function hal_service:_control_main()
     local cap_ctrl_sub, cap_sub_err = self.conn:subscribe({ 'hal', 'capability', '+', '+', 'control', '+' })
@@ -254,7 +277,7 @@ function hal_service:_control_main()
             cap_ctrl_sub:next_msg_op():wrap(function(msg) self:_handle_capability_control(msg) end),
             self.device_event_q:get_op():wrap(function(msg) self:_handle_device_connection_event(msg) end),
             self.ctx:done_op(),
-            unpack(self.events:get_events())
+            self.capability_info_q:get_op():wrap(function(msg) self:_handle_capbility_info(msg) end)
         ):perform()
     end
     cap_ctrl_sub:unsubscribe()
@@ -274,7 +297,7 @@ function hal_service:start(ctx, conn)
     end)
 
     -- start modem manager and detection
-    self.modem_manager_instance:spawn(ctx, conn, self.device_event_q)
+    self.modem_manager_instance:spawn(ctx, conn, self.device_event_q, self.capability_info_q)
 end
 
 return hal_service
