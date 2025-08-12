71b8520f7058188462e5b44d55c93dda56ec2c48
Ryan Name <ryan.slater157@gmail.com>
2025-08-12 10:45:29 +0000

    add UCI capability functions for command handling

--- src/services/hal/hal_capabilities.lua
+++ src/services/hal/hal_capabilities.lua
@@ -68,6 +68,64 @@ local function new_time_capability(driver_q)
     return setmetatable({ driver_q = driver_q }, TimeCapability)
 end
 
+-- uci cap
+local UCICapability = {}
+UCICapability.__index = UCICapability
+
+local function new_uci_capability(driver_q)
+    return setmetatable({driver_q = driver_q}, UCICapability)
+end
+
+function UCICapability:get(args)
+    local cmd = {command = "get", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:set(args)
+    local cmd = {command = "set", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:delete(args)
+    local cmd = {command = "delete", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:commit(args)
+    local cmd = {command = "commit", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:show(args)
+    local cmd = {command = "show", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:add(args)
+    local cmd = {command = "add", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:revert(args)
+    local cmd = {command = "revert", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:changes(args)
+    local cmd = {command = "changes", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:foreach(args)
+    local cmd = {command = "foreach", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UCICapability:set_restart_policy(args)
+    local cmd = {command = "set_restart_policy", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
 -- ubus cap
 local UBusCapability = {}
 UBusCapability.__index = UBusCapability
@@ -105,5 +163,41 @@ return {
     new_modem_capability = new_modem_capability,
     new_ubus_capability = new_ubus_capability,
     new_geo_capability = new_geo_capability,
-    new_time_capability = new_time_capability
+    new_time_capability = new_time_capability,
+    new_uci_capability = new_uci_capability
+}
+    return setmetatable({driver_q = driver_q}, UBusCapability)
+end
+
+function UBusCapability:list()
+    local cmd = {command = "list"}
+    return do_command(self.driver_q, cmd)
+end
+
+function UBusCapability:call(args)
+    local cmd = {command = "call", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UBusCapability:listen(args)
+    local cmd = {command = "listen", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UBusCapability:stop_stream(args)
+    local cmd = {command = "stop_stream", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+function UBusCapability:send(args)
+    local cmd = {command = "send", args = args}
+    return do_command(self.driver_q, cmd)
+end
+
+return {
+    new_modem_capability = new_modem_capability,
+    new_ubus_capability = new_ubus_capability,
+    new_geo_capability = new_geo_capability,
+    new_time_capability = new_time_capability,
+    new_uci_capability = new_uci_capability
 }
