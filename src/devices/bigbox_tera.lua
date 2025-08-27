8da9d4dfba3ba405764f6b01f7eb55575efc7e5e
Ryan Name <ryan.slater157@gmail.com>
2025-08-27 14:25:10 +0000

    configs

---
+++ src/devices/bigbox_tera.lua
@@ -0,0 +1,17 @@
+-- Big Box v0.9 device configuration
+
+return {
+    services = {
+        "log",
+        "config",
+        "hal",
+        "gsm",
+        "time",
+        "net",
+        "metrics",
+        "system",
+        "ui",
+        "wifi"
+        -- ... other services specific to this device version
+    }
+}
