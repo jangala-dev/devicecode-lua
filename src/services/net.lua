3d302f8cf2f1614c56ec42021b8b83b7ba9a7ffc
Richard Thanki <richardthanki@gmail.com>
2025-10-14 17:49:55 +0000

    corrects net to add listen_addresses as lists, neatens

--- src/services/net.lua
+++ src/services/net.lua
@@ -229,28 +229,6 @@ end
 local function set_network_base_config(net_cfg)
     log.info("NET: Applying base network config")
 
-    -- Loopback
-    net_service.conn:publish(new_msg(
-        { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-        { "network", "loopback", "interface" }
-    ))
-    net_service.conn:publish(new_msg(
-        { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-        { "network", "loopback", "device", "lo" }
-    ))
-    net_service.conn:publish(new_msg(
-        { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-        { "network", "loopback", "proto", "static" }
-    ))
-    net_service.conn:publish(new_msg(
-        { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-        { "network", "loopback", "ipaddr", "127.0.0.1" }
-    ))
-    net_service.conn:publish(new_msg(
-        { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-        { "network", "loopback", "netmask", "255.0.0.0" }
-    ))
-
     -- Globals
     net_service.conn:publish(new_msg(
         { 'hal', 'capability', 'uci', '1', 'control', 'set' },
@@ -393,42 +371,49 @@ local function set_network_config(instance)
     log.info("NET: Applying network config for:", net_id)
 
     -- 1. Network interface config
-    if net_cfg.type == "local" then
-        local devicename_sub = net_service.conn:request(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'add' },
-            { "network", "device" }
-        ))
-        local ret = devicename_sub:next_msg_with_context(context.with_timeout(net_service.ctx, BUS_TIMEOUT))
-        devicename_sub:unsubscribe()
-        local devicename, err = ret.payload.result, ret.payload.err
-        if err then
-            log.error("NET: Failed to add network device:", err)
-            return
-        end
-        net_service.conn:publish(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-            { "network", devicename, "name", "br-" .. net_id }
-        ))
-        net_service.conn:publish(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-            { "network", devicename, "type", "bridge" }
-        ))
-        net_service.conn:publish(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-            { "network", devicename, "ports", net_cfg.interfaces or {} }
-        ))
-    end
-
     net_service.conn:publish(new_msg(
         { 'hal', 'capability', 'uci', '1', 'control', 'set' },
         { "network", net_id, "interface" }
     ))
+
     if net_cfg.type == "local" then
-        net_service.conn:publish(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-            { "network", net_id, "device", "br-" .. net_id }
-        ))
-    elseif net_cfg.type == "backhaul" then
+        if net_cfg.is_bridge then
+            local devicename_sub = net_service.conn:request(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'add' },
+                { "network", "device" }
+            ))
+            local ret = devicename_sub:next_msg_with_context(context.with_timeout(net_service.ctx, BUS_TIMEOUT))
+            devicename_sub:unsubscribe()
+            local devicename, err = ret.payload.result, ret.payload.err
+            if err then
+                log.error("NET: Failed to add network device:", err)
+                return
+            end
+            net_service.conn:publish(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'set' },
+                { "network", devicename, "name", "br-" .. net_id }
+            ))
+            net_service.conn:publish(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'set' },
+                { "network", devicename, "type", "bridge" }
+            ))
+            net_service.conn:publish(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'set' },
+                { "network", devicename, "ports", net_cfg.interfaces or {} }
+            ))
+            net_service.conn:publish(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'set' },
+                { "network", net_id, "device", "br-" .. net_id }
+            ))
+        else
+            net_service.conn:publish(new_msg(
+                { 'hal', 'capability', 'uci', '1', 'control', 'set' },
+                { "network", net_id, "device", net_cfg.interfaces[1] }
+            ))
+        end
+    end
+
+    if net_cfg.type == "backhaul" then
         net_service.conn:publish(new_msg(
             { 'hal', 'capability', 'uci', '1', 'control', 'set' },
             { "network", net_id, "peerdns", "0" }
@@ -470,7 +455,7 @@ local function set_network_config(instance)
     end
 
     -- 2. DHCP
-    if net_cfg.dhcp_server then
+    if net_cfg.type=='local' and net_cfg.dhcp_server then
         net_service.conn:publish(new_msg(
             { 'hal', 'capability', 'uci', '1', 'control', 'set' },
             { "dhcp", net_id, "dhcp" }
@@ -491,7 +476,7 @@ local function set_network_config(instance)
             { 'hal', 'capability', 'uci', '1', 'control', 'set' },
             { "dhcp", net_id, "leasetime", net_cfg.dhcp_server.lease_time or "12h" }
         ))
-    else
+    elseif net_cfg.type=='backhaul' then
         net_service.conn:publish(new_msg(
             { 'hal', 'capability', 'uci', '1', 'control', 'set' },
             { "dhcp", net_id, "dhcp" }
@@ -520,10 +505,7 @@ local function set_network_config(instance)
             { 'hal', 'capability', 'uci', '1', 'control', 'set' },
             { "dhcp", instance.dns_id, "domain", net_id }
         ))
-        net_service.conn:publish(new_msg(
-            { 'hal', 'capability', 'uci', '1', 'control', 'set' },
-            { "dhcp", instance.dns_id, "listen_address", { net_cfg.ipv4.ip_address } }
-        ))
+        add_to_uci_list("dhcp", instance.dns_id, "listen_address", net_cfg.ipv4.ip_address)
         -- add_to_uci_list("dhcp", instance.dns_id, "interface", net_id)
     end
 
