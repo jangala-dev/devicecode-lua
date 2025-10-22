3054b90f52b46eb536da889fd983b02519842964
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2025-10-22 11:39:51 +0100

    fixed model import path (#138)

--- src/services/hal/drivers/modem/model.lua
+++ src/services/hal/drivers/modem/model.lua
@@ -2,7 +2,7 @@
 
 local function add_model_funcs(modem)
     local manufacturer = modem.manufacturer
-    local status, model_funcs = pcall(require, "services.gsm.modem_driver.model." .. manufacturer)
+    local status, model_funcs = pcall(require, "services.hal.drivers.modem.model." .. manufacturer)
     if not status then
         return false, "Model functions not found for manufacturer: " .. modem.manufacturer
     end
