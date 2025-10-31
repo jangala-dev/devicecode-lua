c2a93f8197bad2857b4ea02ee14912c5e6f97941
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2025-10-31 13:43:42 +0000

    Wifi Metrics (#149)
    * Added report metrics fiber for each radio
    
    * Fixed missing functions on gen
    
    * fixed wrong mac, pulled interface metrics into function
    
    * fixed client connected bool value in wireless driver
    
    * ssid name now binds to a index for metrics, to keep same conventions as getbox cloud
    
    * Flipped ssid load order, added wifi metrics
    
    * changed client event to realtime
    
    * wifi now tracks number of clients total, per device and per interface
    
    * Added num_sta metrics
    
    * added noise stat collection
    
    * added parsing of noise
    
    * removed debug prints
    
    * added noise to iw driver
    
    * Added hostname reporting
    
    * Added docs on wifi metrics
    
    * removed per client noise
    
    * remove noise per client

--- src/services/hal/drivers/wireless/iw.lua
+++ src/services/hal/drivers/wireless/iw.lua
@@ -23,8 +23,18 @@ local function get_client_info(ctx, interface, mac)
     return utils.format_iw_client_info(out)
 end
 
+local function get_dev_noise(ctx, interface)
+    local out, err = exec.command_context(ctx, "iw", interface, "survey", "dump"):output()
+    if err then
+        return nil, err
+    end
+
+    return utils.parse_dev_noise(out)
+end
+
 return {
     get_iw_dev_info = get_iw_dev_info,
     get_iw_event_stream = get_iw_event_stream,
-    get_client_info = get_client_info
+    get_client_info = get_client_info,
+    get_dev_noise = get_dev_noise
 }
