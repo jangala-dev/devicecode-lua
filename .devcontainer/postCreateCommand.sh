304b87693b475639de8d65925d648136c51c9a0b
Rich Thanki <richardthanki@gmail.com>
2026-01-21 00:55:25 +0000

    updates dev environment

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+cd /tmp
+git clone https://github.com/jangala-dev/lua-fibers
+cd lua-fibers
+git checkout v0.4
+sudo cp -r src/* /workspaces/lua-bus/src/
+cd ..
+rm -rf lua-fibers
+
+cd /tmp
+git clone https://github.com/jangala-dev/lua-trie
+cd lua-trie
+git checkout v0.2
+sudo mv src/trie.lua /workspaces/lua-bus/src/
+cd ..
+rm -rf lua-trie
+
 exit 0
