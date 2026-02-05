024a3c5df885be9d643b775b1e6d7ac18a86189a
Ryan Name <ryan.slater157@gmail.com>
2026-02-05 16:21:22 +0000

    Moved and annotated cache

---
+++ src/shared/cache.lua
@@ -0,0 +1,78 @@
+---@alias CacheKey string|string[]
+---@alias CacheEntry {value: any, timestamp: number, timeout: number}
+
+---@class Cache
+---@field default_timeout number
+---@field time_func function
+---@field separator string
+---@field store table<string, CacheEntry>
+local Cache = {}
+Cache.__index = Cache
+
+--- Cache constructor
+---@param default_timeout number?
+---@param custom_time_func function?
+---@param separator string?
+---@return Cache
+local function new(default_timeout, custom_time_func, separator)
+    local self = setmetatable({}, Cache)
+    self.default_timeout = default_timeout or 10
+    self.time_func = custom_time_func or os.time
+    self.separator = separator or string.char(31)
+    self.store = {}
+    return self
+end
+
+--- Utility function to check if a table is an array
+---@param table table
+local function is_array(table)
+    local i = 0
+    for _ in pairs(table) do
+        i = i + 1
+        if table[i] == nil then return false end
+    end
+    return true
+end
+
+--- Setting a value in the cache
+---@param key string
+---@param value any
+---@param timeout number?
+function Cache:set(key, value, timeout)
+    timeout = timeout or self.default_timeout
+    if type(value) == 'table' then
+        if is_array(value) then
+            self.store[key] = {value=value, timestamp=self.time_func() + timeout}
+        else
+            for k, v in pairs(value) do
+                self:set(key .. self.separator .. k, v, timeout)
+            end
+        end
+    else
+        self.store[key] = {value = value, timestamp=self.time_func(), timeout = timeout}
+    end
+end
+
+--- Getting a value from the cache
+---@param key CacheKey
+---@param timeout number?
+---@return any
+function Cache:get(key, timeout)
+    if stale == nil then stale = false end
+    if type(key) ~= 'string' then
+        key = table.concat(key, self.separator)
+    end
+    local item = self.store[key]
+    if item then
+        timeout = timeout or item.timeout
+        if self.time_func() < (item.timestamp + timeout) then
+            return item.value
+        end
+    end
+    return nil -- or a default value
+end
+
+return {
+    new = new,
+    Cache = Cache
+}
