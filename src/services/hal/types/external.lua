d3386c95fb06dd0cc287f69a2ebedd1fab1b8d92
Ryan Name <ryan.slater157@gmail.com>
2026-02-20 17:10:40 +0000

    Added filesystem opts

--- src/services/hal/types/external.lua
+++ src/services/hal/types/external.lua
@@ -16,7 +16,7 @@ function new.ModemGetOpts(field, timescale)
         return nil, "invalid field"
     end
 
-    if timescale ~= nil and (type(timescale) ~= 'number' or timescale <= 0) then
+    if timescale ~= nil and (type(timescale) ~= 'number' or timescale < 0) then
         return nil, "invalid timescale"
     end
 
@@ -62,8 +62,74 @@ function new.ModemSignalUpdateOpts(frequency)
     }, ModemSignalUpdateOpts), ""
 end
 
+---@class FilesystemReadOpts
+---@field filename string
+local FilesystemReadOpts = {}
+FilesystemReadOpts.__index = FilesystemReadOpts
+
+--- Validate that a filename contains no path separators or .. segments
+---@param filename string
+---@return boolean valid
+---@return string? error
+local function validate_filename(filename)
+    if type(filename) ~= 'string' or filename == '' then
+        return false, "filename must be a non-empty string"
+    end
+
+    if filename:find('/') or filename:find('\\') then
+        return false, "filename cannot contain path separators"
+    end
+
+    if filename == '..' or filename:find('^%.%.') or filename:find('%.%.') then
+        return false, "filename cannot contain .. segments"
+    end
+
+    return true, nil
+end
+
+---Create a new FilesystemReadOpts
+---@param filename string
+---@return FilesystemReadOpts?
+---@return string error
+function new.FilesystemReadOpts(filename)
+    local valid, err = validate_filename(filename)
+    if not valid then
+        return nil, err
+    end
+    return setmetatable({
+        filename = filename,
+    }, FilesystemReadOpts), ""
+end
+
+---@class FilesystemWriteOpts
+---@field filename string
+---@field data string
+local FilesystemWriteOpts = {}
+FilesystemWriteOpts.__index = FilesystemWriteOpts
+
+---Create a new FilesystemWriteOpts
+---@param filename string
+---@param data string
+---@return FilesystemWriteOpts?
+---@return string error
+function new.FilesystemWriteOpts(filename, data)
+    local valid, err = validate_filename(filename)
+    if not valid then
+        return nil, err
+    end
+    if type(data) ~= 'string' then
+        return nil, "invalid data"
+    end
+    return setmetatable({
+        filename = filename,
+        data = data,
+    }, FilesystemWriteOpts), ""
+end
+
 return {
     ModemGetOpts = ModemGetOpts,
     ModemConnectOpts = ModemConnectOpts,
+    FilesystemReadOpts = FilesystemReadOpts,
+    FilesystemWriteOpts = FilesystemWriteOpts,
     new = new,
 }
