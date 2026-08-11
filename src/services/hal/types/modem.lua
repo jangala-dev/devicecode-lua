8c388985b9119d0f06a4097a9e2f1bdf70289585
Ryan Name <ryan.slater157@gmail.com>
2026-08-11 15:12:50 +0000

    Signal can now be empty table

--- src/services/hal/types/modem.lua
+++ src/services/hal/types/modem.lua
@@ -188,7 +188,6 @@ local function is_signal_value_table(value)
 		return false
 	end
 
-	local has_tech = false
 	for tech, signals in pairs(value) do
 		if type(tech) ~= 'string' or type(signals) ~= 'table' then
 			return false
@@ -204,9 +203,8 @@ local function is_signal_value_table(value)
 		if not has_signal then
 			return false
 		end
-		has_tech = true
 	end
-	return has_tech
+	return true
 end
 
 ---Create a new ModemIdentityInfo.
